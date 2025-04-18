beaa93bec9739f2eef1df04c5f771f91b9fd1e6e
GabrielAlbernazDev <gabrielalbernaz22@gmail.com>
2025-04-18 10:56:40 -0300

    Fix logout inside a li

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,10 +1,18 @@
 (function(){
     document.addEventListener('DOMContentLoaded', function() {
         const profileContainer = document.querySelector('.profile-container');
+        const logoutContainers = document.querySelectorAll('.logout-container');
+        const logoutBtn = document.querySelector('.logout');
         
         profileContainer.addEventListener('click', (event) => {
             profileContainer.classList.toggle('active');
             event.stopPropagation();
+        });
+
+        logoutContainers.forEach(logoutContainer => {
+            logoutContainer.addEventListener('click', () => {
+                logoutBtn?.click();
+            });
         });
         
         document.addEventListener('click', () => {
